87f2fcc79abda7383c035bb4781254cfcf9ddab1
Şeref ÖZBAŞ <serefo@med-case.io>
2024-05-16 14:05:32 +0300

    4521-react-slider kütüphanesi yüklendi ve uygulandı.

--- Helios.Frontend/Helios.eCRF/ClientApp/src/Pages/TenantAdmin/Module/Elements/RangeSliderElement/rangeSliderElement.js
+++ Helios.Frontend/Helios.eCRF/ClientApp/src/Pages/TenantAdmin/Module/Elements/RangeSliderElement/rangeSliderElement.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import Slider from 'react-rangeslider';
+import Slider from 'react-slider';
 import 'react-rangeslider/lib/index.css';
 import './rangeSliderElement.css';
+
 
 const sliderRef = '0px';
 class RangeSliderElement extends Component {
     constructor(props) {
         super(props);
-        
         this.state = {
             isDisable: props.IsDisable,
             horizontal: props.DefaultValue,
@@ -28,24 +28,30 @@ class RangeSliderElement extends Component {
             horizontal: value
         })
 
-    };
-   
+    };  
+  
     render() {
         return (
             <div className='slider custom-labels'>
-                <p className='defaultvalue'>{this.state.horizontal !== "" ? parseInt(this.state.horizontal) : parseInt(this.props.LowerLimit)}</p>
+                <p className='defaultvalue'>{this.state.horizontal !== "" ? parseInt(this.state.horizontal) : parseInt(this.props.LowerLimit)}</p>              
                 <Slider
                     ref={this.myRef}
                     min={parseInt(this.props.LowerLimit)}
                     max={parseInt(this.props.UpperLimit)}
-                    value={this.state.horizontal !== "" ? parseInt(this.state.horizontal) : parseInt(this.props.LowerLimit) }
-                    labels={this.state.horizontalLabels}
-                    onChange={this.handleChangeHorizontal}
-                />
+                    value={this.state.horizontal !== "" ? parseInt(this.state.horizontal) : parseInt(this.props.LowerLimit)}
+                    getAriaValueText={this.state.horizontalLabels}                  
+                    onChange={this.handleChangeHorizontal}                   
+                    disabled={this.state.isDisable}
+                    className="customSlider"
+                    trackClassName="customSlider-track"
+                    thumbClassName="customSlider-thumb"
+                    />              
                 <div className='row'>
                     <p className='LowerLimitString'>{parseInt(this.props.LowerLimit)} </p>
                     <p className='UpperLimitString'>{parseInt(this.props.UpperLimit)}</p>
-                </div>
+                    <p className='LeftText'>{this.props.LeftText} </p>
+                    <p className='RightText'>{this.props.RightText}</p>
+                </div>               
             </div>
         )
     }
