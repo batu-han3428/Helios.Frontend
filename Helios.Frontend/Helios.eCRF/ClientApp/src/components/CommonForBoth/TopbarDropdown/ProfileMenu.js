5b10f3ba5520ea3a59c663b4d8733ed7b26b8dd7
Şeref ÖZBAŞ <serefo@med-case.io>
2024-06-03 13:54:19 +0300

    4611-Admin / Profil dropdown'a "Switch to tenant / Aboneliği değiştir" butonu eklenecek.

--- Helios.Frontend/Helios.eCRF/ClientApp/src/components/CommonForBoth/TopbarDropdown/ProfileMenu.js
+++ Helios.Frontend/Helios.eCRF/ClientApp/src/components/CommonForBoth/TopbarDropdown/ProfileMenu.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from 'prop-types';
 import {
-  Dropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
+    Dropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
 } from "reactstrap";
 
 //i18n
@@ -18,75 +18,86 @@ import withRouter from "../../Common/withRouter";
 import user1 from "../../../assets/images/users/user-4.jpg";
 
 const ProfileMenu = props => {
-  // Declare a new state variable, which we'll call "menu"
-  const [menu, setMenu] = useState(false);
+    // Declare a new state variable, which we'll call "menu"
+    const [menu, setMenu] = useState(false);
 
-  const [username, setusername] = useState("Admin");
+    const [username, setusername] = useState("Admin");
 
-  useEffect(() => {
-    if (localStorage.getItem("authUser")) {
-      if (process.env.REACT_APP_DEFAULTAUTH === "firebase") {
-        const obj = JSON.parse(localStorage.getItem("authUser"));
-        setusername(obj.displayName);
-      } else if (
-        process.env.REACT_APP_DEFAULTAUTH === "fake" ||
-        process.env.REACT_APP_DEFAULTAUTH === "jwt"
-      ) {
-        const obj = JSON.parse(localStorage.getItem("authUser"));
-        setusername(obj.username);
-      }
-    }
-  }, [props.success]);
+    const roletatu = props.roles.length > 0 ? props.roles.includes('TenantAdmin') : false;
+    
+    useEffect(() => {
+        if (localStorage.getItem("authUser")) {
+            if (process.env.REACT_APP_DEFAULTAUTH === "firebase") {
+                const obj = JSON.parse(localStorage.getItem("authUser"));
+                setusername(obj.displayName);
+            } else if (
+                process.env.REACT_APP_DEFAULTAUTH === "fake" ||
+                process.env.REACT_APP_DEFAULTAUTH === "jwt"
+            ) {
+                const obj = JSON.parse(localStorage.getItem("authUser"));
+                setusername(obj.username);
+            }
+        }
+    }, [props.success]);
 
-  return (
-    <React.Fragment>
-      <Dropdown
-        isOpen={menu}
-        toggle={() => setMenu(!menu)}
-        className="d-inline-block"
-      >
-        <DropdownToggle
-          className="btn header-item waves-effect"
-          id="page-header-user-dropdown"
-          tag="button"
-        >
-          <img
-            className="rounded-circle header-profile-user"
-            src={user1}
-            alt="Header Avatar"
-          />
-        </DropdownToggle>
-        <DropdownMenu className="dropdown-menu-end">
-          <DropdownItem tag="a" href="/profile">
-            {" "}
-            <i className="bx bx-user font-size-16 align-middle me-1" />
-            {props.t("Profile")}{" "}
-          </DropdownItem>
-          <DropdownItem tag="a" href="auth-lock-screen">
-            <i className="bx bx-lock-open font-size-16 align-middle me-1" />
-            {props.t("Lock screen")}
-          </DropdownItem>
-          <div className="dropdown-divider" />
-          <Link to="/logout" className="dropdown-item">
-            <i className="bx bx-power-off font-size-16 align-middle me-1 text-danger" />
-            <span>{props.t("Logout")}</span>
-          </Link>
-        </DropdownMenu>
-      </Dropdown>
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <Dropdown
+                isOpen={menu}
+                toggle={() => setMenu(!menu)}
+                className="d-inline-block"
+            >
+                <DropdownToggle
+                    className="btn header-item waves-effect"
+                    id="page-header-user-dropdown"
+                    tag="button"
+                >
+                    <img
+                        className="rounded-circle header-profile-user"
+                        src={user1}
+                        alt="Header Avatar"
+                    />
+                </DropdownToggle>
+                <DropdownMenu className="dropdown-menu-end">
+                    <DropdownItem tag="a" href="/profile">
+                        {" "}
+                        <i className="bx bx-user font-size-16 align-middle me-1" />
+                        {props.t("Profile")}{" "}
+                    </DropdownItem>
+                    {
+                        roletatu &&
+                        <DropdownItem tag="a" href="/SSO-tenants/3">
+                            <i className="bx bx-user font-size-16 align-middle me-1" />
+                            {props.t("Switch to tenant")}{" "}
+                        </DropdownItem>
+                    }
+                   
+                    <DropdownItem tag="a" href="auth-lock-screen">
+                        <i className="bx bx-lock-open font-size-16 align-middle me-1" />
+                        {props.t("Lock screen")}
+                    </DropdownItem>
+                    <div className="dropdown-divider" />
+                    <Link to="/logout" className="dropdown-item">
+                        <i className="bx bx-power-off font-size-16 align-middle me-1 text-danger" />
+                        <span>{props.t("Logout")}</span>
+                    </Link>
+                </DropdownMenu>
+            </Dropdown>
+        </React.Fragment>
+    );
 };
 
 ProfileMenu.propTypes = {
-  success: PropTypes.any,
-  t: PropTypes.any
+    success: PropTypes.any,
+    t: PropTypes.any
 };
 
 const mapStatetoProps = state => {
     const { error, success } = state.rootReducer.Profile;
-  return { error, success };
+    const roles = state.rootReducer.Login.roles;
+    return { error, success, roles };
 };
 
 export default withRouter(
-  connect(mapStatetoProps, {})(withTranslation()(ProfileMenu))
+    connect(mapStatetoProps, {})(withTranslation()(ProfileMenu))
 );
