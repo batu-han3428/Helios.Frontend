162abc29cdf042758bca3514ee22c6254be54568
Somayeh JANGI <somayehj@med-case.io>
2024-07-09 13:42:11 +0300

    Task 4677: Numeric element changed.

--- Helios.Frontend/Helios.eCRF/ClientApp/src/Pages/TenantAdmin/Module/Elements/NumericElement/numericElement.js
+++ Helios.Frontend/Helios.eCRF/ClientApp/src/Pages/TenantAdmin/Module/Elements/NumericElement/numericElement.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { Tooltip } from 'react-tooltip';
+import { withTranslation } from "react-i18next";
 
 class NumericElement extends Component {
     constructor(props) {
         super(props);
-        
+
         this.state = {
             id: props.Id,
             isDisable: props.IsDisable,
@@ -11,51 +13,90 @@ class NumericElement extends Component {
             Mask: props.Mask,
             LowerLimit: props.LowerLimit,
             UpperLimit: props.UpperLimit,
-            Value: props.Value
+            Value: props.Value,
+            tooltipMessage: '',
+            showTooltip: false
         }
 
         this.handleChange = this.handleChange.bind(this);
         this.handleBlur = this.handleBlur.bind(this);
+        this.validateValue = this.validateValue.bind(this);
     }
 
-    handleChange = (e) => {
-        let newValue = parseInt(e.target.value, 10);
+    handleChange(e) {
+        let newValue = e.target.value;
+        const regex = /^[0-9]*\.?[0-9]*$/;
+
+        if (regex.test(newValue)) {
+            this.setState({
+                Value: newValue,
+                showTooltip: false,
+                tooltipMessage: ''
+            });
+        }
+    }
+
+    handleBlur(e) {
+        this.validateValue(e.target.value);
+    }
+
+    validateValue(value) {
+        let newValue = parseFloat(value);
+        let tooltipMessage = '';
 
         if (isNaN(newValue)) {
-            newValue = this.LowerLimit;
+            newValue = this.state.LowerLimit;
         }
 
-        if (newValue < this.LowerLimit) {
-            newValue = this.LowerLimit;
-        }
-
-        if (newValue > this.UpperLimit) {
-            newValue = this.UpperLimit;
-        }
+        if (this.state.LowerLimit !== this.state.UpperLimit)
+            if (newValue < this.state.LowerLimit) {
+                tooltipMessage = this.props.t("Value should not be less than") + this.state.LowerLimit + this.props.t("and greater than") + this.state.UpperLimit;
+                this.setState({
+                    showTooltip: true,
+                    tooltipMessage
+                });
+            } else if (newValue > this.state.UpperLimit) {
+                tooltipMessage = this.props.t("Value should not be less than") + this.state.LowerLimit + this.props.t("and greater than") + this.state.UpperLimit;
+                this.setState({
+                    showTooltip: true,
+                    tooltipMessage
+                });
+            } else {
+                this.setState({
+                    showTooltip: false,
+                    tooltipMessage: ''
+                });
+            }
 
         this.setState({
-            Value: newValue,
+            Value: value
         });
-    };
 
-    handleBlur = async (e) => {
-        this.props.HandleAutoSave(this.state.id, e.target.value);
-    };
+        this.props.HandleAutoSave(this.state.id, value);
+    }
 
     render() {
         return (
-            <div style={{ marginRight: "20px" }} >
+            <div style={{ marginRight: "20px" }}>
                 <input
                     className="form-control"
                     type="number"
+                    step="0.01"
                     disabled={this.state.isDisable}
                     value={this.state.Value}
                     onChange={this.handleChange}
                     onBlur={this.handleBlur}
+                    //min={this.state.LowerLimit}
+                    //max={this.state.UpperLimit}
+                    data-tooltip-id={`tooltip-${this.state.id}`}
+                    data-tooltip-content={this.state.tooltipMessage}
                 />
+                {this.state.showTooltip && (
+                    <Tooltip id={`tooltip-${this.state.id}`} place="top" effect="solid" type="error" />
+                )}
             </div>
         )
     }
-};
+}
 
-export default NumericElement;
+export default withTranslation()(NumericElement);
